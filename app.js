e08914475f2c18a943d4c8404a7ac676a0e8971a
Amusah <henryamusah@gmail.com>
2023-08-24 11:32:50 +0000

    added mobile nav toggler and hero image slider

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('.nav-links');
 const burger = document.querySelector('.hamburger');
 const close = document.querySelector('.close-btn');
+const heroSlider = document.getElementById('hero');
+const sliderButton = document.querySelectorAll('.btn');
 
 
 // nav toggle functionality
@@ -9,3 +11,50 @@ const navToggler = () => {
 }
 burger.addEventListener('click', navToggler);
 close.addEventListener('click', navToggler);
+
+
+const screens = {
+        mobile: [
+            '/images/mobile-image-hero-1.jpg',
+            '/images/mobile-image-hero-2.jpg',
+            '/images/mobile-image-hero-3.jpg'
+        ],
+        desktop: [
+            '/images/desktop-image-hero-1.jpg',
+            '/images/desktop-image-hero-2.jpg',
+            '/images/desktop-image-hero-3.jpg'
+        ]
+    };
+
+const changeImage = (num) => {
+    const imgageSource = window.innerWidth < 500 ? screens.mobile[num] : screens.desktop[num];
+    heroSlider.style.transition = '1s ease-out';
+    heroSlider.style.backgroundImage = `url(${imgageSource})`;
+}
+
+let counter = 0;
+sliderButton.forEach(btn => {
+    btn.addEventListener('click', ({ target }) => {
+        if(target.classList.contains('next-slide')){
+            if(counter === 2){
+                counter = 0;
+                changeImage(counter);
+            }else {
+                counter++;
+                changeImage(counter);
+            }
+            // console.log(counter)
+        }
+
+        if(target.classList.contains('prev-slide')){
+            if(counter === 0){
+                counter = 2;
+                changeImage(counter);
+            }else {
+                counter--;
+                changeImage(counter);
+            }
+            // console.log(counter)
+        }
+    })
+});
